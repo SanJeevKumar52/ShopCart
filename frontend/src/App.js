8bd856385f3600083f0485bb2110c63aa698a35c
Sanjeev <sk3616593@gmail.com>
2024-03-01 20:31:58 +0530

    React router added succesfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/Homescreen";
+
 
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen/>
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
       <Footer/>
     </>
   )
