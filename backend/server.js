72d14c3032055cd85fcd2e2bfa6d0bcf2b3889a8
Sanjeev <sk3616593@gmail.com>
2024-03-01 23:11:29 +0530

    setup of nodemon ,concurrently and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import  dotenv from 'dotenv';
+
 import express from 'express';
 import products from './data/products.js';
-const port = 5000;
+const port = process.env.PORT||5000;
 
 const app = express();
 
