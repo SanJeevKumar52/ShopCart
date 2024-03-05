da15853a0497e592251826cf3cb2ff7c13436d49
Sanjeev <sk3616593@gmail.com>
2024-03-05 21:46:25 +0530

    Setup of redux toolkit and slices done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,9 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Provider } from 'react-redux';
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/index.css'
 /* import './index.css'; */
 import App from './App';
@@ -14,9 +16,9 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-       <Route index ={true} path='/' element={<HomeScreen/>}/>
-       <Route path='/product/:id' element={<ProductScreen/>}/>
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
@@ -24,7 +26,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
