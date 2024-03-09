5d1c060b7e33d432c3ed390b9de4a70e12e642ed
Sanjeev <sk3616593@gmail.com>
2024-03-10 01:14:07 +0530

    Auth middleware and ennpoint Added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,7 +11,7 @@ import {
     getUserById,
     updateUser,
 } from '../controllers/userController.js'; // Importing functions from userController.js module
-
+import {protect, admin} from '../middleware/authMiddleware.js';
 // Creating an instance of Express's Router
 const router = express.Router();
 
@@ -19,20 +19,20 @@ const router = express.Router();
 router
     .route('/') // Define routes for user registration and retrieving all users
     .post(registerUser) // For HTTP POST requests to '/', invoke registerUser function
-    .get(getUsers); // For HTTP GET requests to '/', invoke getUsers function
+    .get(protect,admin,getUsers); // For HTTP GET requests to '/', invoke getUsers function
 
-router.post('/login', authUser); // Define a route for user authentication
+router.post('/auth', authUser); // Define a route for user authentication
 
 router
     .route('/profile') // Define routes for user profile operations
-    .get(getUserProfile) // For HTTP GET requests to '/profile', invoke getUserProfile function
-    .put(updateUserProfile); // For HTTP PUT requests to '/profile', invoke updateUserProfile function
+    .get(protect,getUserProfile) // For HTTP GET requests to '/profile', invoke getUserProfile function
+    .put(protect,updateUserProfile); // For HTTP PUT requests to '/profile', invoke updateUserProfile function
 
 router
     .route('/:id') // Define routes for operations on individual users identified by their ID
-    .delete(deleteUser) // For HTTP DELETE requests to '/:id', invoke deleteUser function
-    .get(getUserById) // For HTTP GET requests to '/:id', invoke getUserById function
-    .put(updateUser); // For HTTP PUT requests to '/:id', invoke updateUser function
+    .delete(protect,admin,deleteUser) // For HTTP DELETE requests to '/:id', invoke deleteUser function
+    .get(protect,admin,getUserById) // For HTTP GET requests to '/:id', invoke getUserById function
+    .put(protect,admin,updateUser); // For HTTP PUT requests to '/:id', invoke updateUser function
 router
     .post('/logout', logoutUser);
 // Exporting the router instance as the default export of this module
