ff2255279569634c3a3cb896bd60f84f6335590e
Sanjeev <sk3616593@gmail.com>
2024-04-08 23:05:51 +0530

    paypal setup and order paid setup done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import  dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 
-import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -16,22 +16,25 @@ const app = express();
 
 //body parser-middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //CookieParser middleWare
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
    res.send('API is running');
 })
 
 
 
-app.use('/api/products',productRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
+app.get('/api/config/paypal', (req, res) =>
+   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port,()=> console.log(`Server is running on ${port}`));
+app.listen(port, () => console.log(`Server is running on ${port}`));
