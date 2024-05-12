89802f51a27c52b45d36fbbc65df47a994c0900e
Sanjeev <sk3616593@gmail.com>
2024-05-12 23:21:36 +0530

    Create product by  admin done

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express, { request } from "express";
 const router = express.Router();
-import { getProducts, getProductById } from "../controllers/productController.js";
-
+import { getProducts, getProductById,createProduct } from "../controllers/productController.js";
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 
 
