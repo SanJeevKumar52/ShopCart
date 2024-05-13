74487dff1b2539775dbc92380198e9d516a22e39
Sanjeev <sk3616593@gmail.com>
2024-05-13 23:12:22 +0530

    upload image using multer done

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,37 +1,49 @@
-import { PRODUCTS_URL } from "../constants";
+import { PRODUCTS_URL,UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-      getProducts: builder.query({
-        query: () => ({
-          url: PRODUCTS_URL,
-        }),
-        keepUnusedDataFor: 5,
-        providesTags: ['Products'],
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
       }),
-      getProductDetails: builder.query({
-        query: (productId) => ({
-          url: `${PRODUCTS_URL}/${productId}`,
-        }),
-        keepUnusedDataFor: 5,
+      keepUnusedDataFor: 5,
+      providesTags: ['Products'],
+    }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
       }),
-      createProduct: builder.mutation({
-        query: () => ({
-          url: `${PRODUCTS_URL}`,
-          method: 'POST',
-        }),
-        invalidatesTags: ['Product'],
+      keepUnusedDataFor: 5,
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: `${PRODUCTS_URL}`,
+        method: 'POST',
       }),
-      updateProduct: builder.mutation({
-        query: (data) => ({
-          url: `${PRODUCTS_URL}/${data.productId}`,
-          method: 'PUT',
-          body: data,
-        }),
-        invalidatesTags: ['Products'],
+      invalidatesTags: ['Product'],
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Products'],
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `${UPLOAD_URL}`,
+        method: 'POST',
+        body: data,
       }),
     }),
-  });
+  }),
+});
 
-export const {useGetProductsQuery,useGetProductDetailsQuery,useCreateProductMutation, useUpdateProductMutation,} = productsApiSlice;
+export const { useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation, }
+  = productsApiSlice;
