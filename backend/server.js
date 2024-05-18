9fe3321712ab441c15d3917e432c71f090576532
Sanjeev <sk3616593@gmail.com>
2024-05-18 21:31:55 +0530

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -41,8 +41,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-   /* const __dirname = path.resolve();
-   app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); */
+   const __dirname = path.resolve();
+   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
   app.get('/', (req, res) => {
     res.send('API is running....');
